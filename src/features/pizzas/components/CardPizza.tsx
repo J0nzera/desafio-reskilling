1bb039f8a03b89e4d0a0290db0ac97baacc826e3
J0nzera <jonsilva026@gmail.com>
2026-02-07 17:22:35 -0300

    feat: substituindo tailwind por css module

--- src/features/pizzas/components/CardPizza.tsx
+++ src/features/pizzas/components/CardPizza.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Pizza } from "../interfaces/pizza.interface";
 import { ClockIcon } from "@heroicons/react/24/outline";
 import { formatCurrency } from "@/shared/utils/formatCurrency.util";
+import styles from "./CardPizza.module.css";
 
 type Props = {
   pizza: Pizza;
@@ -9,24 +10,26 @@ type Props = {
 
 export function CardPizza({ pizza }: Props) {
   return (
-    <div className="shadow-md rounded-xl bg-white overflow-hidden w-50">
-      <Image src={pizza.imagem} alt={pizza.nome} width={200} height={200} />
+    <div className={styles.card}>
+      <Image
+        src={pizza.imagem}
+        alt={pizza.nome}
+        width={200}
+        height={200}
+        className={styles.image}
+      />
 
-      <div className="p-2">
-        <p className="text-sm text-black">{pizza.nome}</p>
+      <div className={styles.content}>
+        <p className={styles.name}>{pizza.nome}</p>
 
-        <div className="flex items-center">
-          <ClockIcon className="w-5 h-5 text-green-600 my-2" />
-          <span className="text-xs font-semibold text-green-600 ml-1">
-            {pizza.tempoEntrega}
-          </span>
+        <div className={styles.timeContainer}>
+          <ClockIcon className={styles.timeIcon} />
+          <span className={styles.timeText}>{pizza.tempoEntrega}</span>
         </div>
 
-        <p className="text-xs text-black mb-2">{pizza.descricao}</p>
+        <p className={styles.description}>{pizza.descricao}</p>
 
-        <span className="text-sm font-bold text-black">
-          {formatCurrency(pizza.valor)}
-        </span>
+        <span className={styles.price}>{formatCurrency(pizza.valor)}</span>
       </div>
     </div>
   );
